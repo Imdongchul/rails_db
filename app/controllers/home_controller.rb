333eed1e048e1972b79e9c96b209ceeeb6ff64ff
Imdongchul <cool_93@likelion.org>
2017-05-11 20:56:50 +0900

    최종완료

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
   def index
+  	@qall = Question.all
   end
+
+  def new
+  end
+
+  def craete
+  	q = Question.new
+  	q.name = params[:db_name]
+  	q.content = params[:db_content]
+  	q.save
+  end
+
 end
